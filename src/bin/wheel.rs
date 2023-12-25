bfd309a7d31b94aaf155b602a831416418a65ce1
motii <108280115+motii8128@users.noreply.github.com>
2023-12-25 22:04:37 +0900

    Add files via upload

--- src/bin/wheel.rs
+++ src/bin/wheel.rs
@@ -3,12 +3,45 @@ use zenoh::{
     prelude::r#async::*,
 };
 
-use futures::select;
-
 use async_std;
+use zenoh_interface::{CmdVel, motor_controll::QuadMotor};
+
 
 #[async_std::main]
 async fn main()
 {
     let session = zenoh::open(Config::default()).res().await.unwrap();
+
+    let sub_topic = "cmd_vel".to_string();
+    let pub_topic = "wheel_command".to_string();
+
+    let subscriber = session.declare_subscriber(&sub_topic).res().await.unwrap();
+    let publisher = session.declare_publisher(&pub_topic).res().await.unwrap();
+
+    let diagonal = ((2.0_f32).sqrt() / 2.0) as f32;
+
+
+    loop {
+        let get_data = subscriber.recv_async().await.unwrap();
+
+        let deserialized:CmdVel = serde_json::from_str(&get_data.value.to_string()).unwrap();
+
+        let wheel_cmd = QuadMotor{
+            power_0:deserialized.x*diagonal - deserialized.y*diagonal + deserialized.rotation_power,
+            power_1:deserialized.x*diagonal - deserialized.y*diagonal + deserialized.rotation_power,
+            power_2:-deserialized.x*diagonal - deserialized.y*diagonal + deserialized.rotation_power,
+            power_3:-deserialized.x*diagonal - deserialized.y*diagonal + deserialized.rotation_power,
+        };
+
+        println!("fl:{}, fr:{}, rl:{}, rr:{}", wheel_cmd.power_0, wheel_cmd.power_1, wheel_cmd.power_2, wheel_cmd.power_3);
+
+        let serialized = serde_json::to_string(&wheel_cmd).unwrap();
+
+        publisher.put(serialized).res().await.unwrap();
+    }
 }
+
+
+// wheel
+// 0    1
+// 2    3
