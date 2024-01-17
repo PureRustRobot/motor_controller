1ed04896c27cd62414a8f44e989dfeb0bbd37b46
motii <108280115+motii8128@users.noreply.github.com>
2024-01-17 10:54:04 +0900

    Add files via upload

--- src/lib.rs
+++ src/lib.rs
@@ -4,17 +4,18 @@ use zenoh::{
     Error
 };
 
-use zenoh_interface::{CmdVel, motor_controll::{QuadMotor, SingleMotor}};
+use prr_msgs::msg::*;
 use zenoh_manage_utils::param::{get_str_param, get_bool_param};
 use zenoh_manage_utils::logger;
 
 
-pub async fn wheel_controller(node_name:&str, yaml_path:&str)->Result<(), Error>
+pub async fn wheel_controller(
+    node_name:&str, 
+    sub_topic:&str,
+    pub_topic:&str,
+)->Result<(), Error>
 {
     let session = zenoh::open(Config::default()).res().await.unwrap();
-
-    let sub_topic = get_str_param(yaml_path, node_name, "sub_topic", "cmd_vel".to_string());
-    let pub_topic = get_str_param(yaml_path, node_name, "pub_topic", "output".to_string());
 
     let subscriber = session.declare_subscriber(&sub_topic).res().await.unwrap();
     let publisher = session.declare_publisher(&pub_topic).res().await.unwrap();
@@ -26,18 +27,19 @@ pub async fn wheel_controller(node_name:&str, yaml_path:&str)->Result<(), Error>
 
 
     loop {
-        let get_data = subscriber.recv_async().await.unwrap();
+        let sample = subscriber.recv_async().await.unwrap();
 
-        let deserialized:CmdVel = serde_json::from_str(&get_data.value.to_string()).unwrap();
+        let get_data = deserialize_cmdvel(sample.value.to_string());
 
-        let wheel_cmd = QuadMotor{
-            power_0:deserialized.x*diagonal - deserialized.y*diagonal + deserialized.rotation_power,
-            power_1:deserialized.x*diagonal - deserialized.y*diagonal + deserialized.rotation_power,
-            power_2:-deserialized.x*diagonal - deserialized.y*diagonal + deserialized.rotation_power,
-            power_3:-deserialized.x*diagonal - deserialized.y*diagonal + deserialized.rotation_power,
+        let wheel_cmd = Wheel{
+            front_left:get_data.x*diagonal - get_data.y*diagonal + get_data.rotation_power,
+            front_right:get_data.x*diagonal - get_data.y*diagonal + get_data.rotation_power,
+            rear_left:-get_data.x*diagonal - get_data.y*diagonal + get_data.rotation_power,
+            rear_right:-get_data.x*diagonal - get_data.y*diagonal + get_data.rotation_power,
         };
 
-        let serialized = serde_json::to_string(&wheel_cmd).unwrap();
+        let serialized = serialize_wheel(&wheel_cmd);
+
         let log_data = format!("send :{}", serialized);
 
         logger::log_info(node_name, log_data);
@@ -63,10 +65,10 @@ pub async fn single_controller(node_name:&str, yaml_path:&str)->Result<(), Error
     loop {
         let get_data = subscriber.recv_async().await.unwrap();
 
-        let deserialized:CmdVel = serde_json::from_str(&get_data.value.to_string()).unwrap();
+        let get_data:CmdVel = serde_json::from_str(&get_data.value.to_string()).unwrap();
 
         let mut cmd = SingleMotor{
-            power:deserialized.rotation_power,
+            power:get_data.rotation_power,
         };
 
         if reversal
